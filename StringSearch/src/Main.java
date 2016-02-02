62cf33e6112a0665570906a5d311bcbea9f83a35
emiliev <emil_iliev1994@abv.bg>
2016-02-02 20:11:37 +0200

    fiding .txt files in directory

--- StringSearch/src/Main.java
+++ StringSearch/src/Main.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		
+		Path stringSearch = Paths.get("E:/testing");
+		Files.walk(stringSearch).forEach(filePath -> {
+    		
+    		if (Files.isRegularFile(filePath)  &&  filePath.toString().endsWith(".txt"))
+            {
+                System.out.println(filePath.toString());
+    
+            }
+	
+		});
+		
 	}
-
 }
